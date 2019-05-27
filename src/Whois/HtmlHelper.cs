2e620548ed8eca1f2f226be0cde8146001c8208f
Behzad <behzad.khosravifar@gmail.com>
2019-05-27 22:38:42 +0430

    checkbox html helper created

---
+++ src/Whois/HtmlHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Whois
+{
+    public static class HtmlHelper
+    {
+        public static IHtmlContent BootstrapCheckbox(this IHtmlHelper html, string id, string content, bool isChecked = false, string css = "",  string note = "",
+            string noteClass = "")
+        {
+            return new HtmlString($"<div class=\"control custom-control custom-checkbox {css}\"><input id=\"{id}\" type=\"checkbox\" class=\"custom-control-input\" " +
+                                  (isChecked ? "checked" : "") + ">" +
+                                  $"<label class=\"custom-control-label\" for=\"{id}\"><span class=\"checkbox-text\"><span class=\"checkbox-content\">{content}</span>" +
+                                  (string.IsNullOrEmpty(note) ? "": $"<span class=\"checkbox-note {noteClass}\">(" + note + ")</span>") +
+                                  "</span></label></div>");
+        }
+    }
+}
